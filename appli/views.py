117663839b299d8090de51098d8d0b46b3f781c2
EME2020 <EME2020@ecam-rennes.com>
2020-02-11 10:35:26 +0100

    Oupsie j'ai tout casse

--- appli/views.py
+++ appli/views.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, request
-from Object_detection_image import analyseBoue
+from tensorflow2.models.research.object_detection.Object_detection_image import analyseBoue
+import sys
+
+print( sys.path )
 
 app = Flask(__name__, static_folder="static/images")
 
